2dbbb7dc74754547c0cf7cc8312dfe2081701896
JuliaMatsievich <juliamatsievich@mail.ru>
2023-12-07 15:01:50 +0500

    добавила шрифты

---
+++ src/pages/mainPage/mainPage.tsx
@@ -0,0 +1,9 @@
+import { Main } from '../../components/main/main';
+
+export const MainPage = () => {
+  return (
+    <>
+      <Main />
+    </>
+  );
+};
---
+++ src/styles/globalStyle.styles.tsx
@@ -0,0 +1,45 @@
+import styled, { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+	
+@font-face {
+	font-family: 'Roboto';
+  src: local('Roboto'),
+    url('./fonts/Roboto/Roboto-Regular.ttf') format('truetype');
+  font-weight: 400;
+}
+
+* {
+	margin: 0;
+	padding: 0;
+	box-sizing: border-box;
+}
+
+*:before,
+*:after {
+	box-sizing: border-box;
+}
+ 
+ul li {
+  list-style: none;
+}
+
+html,
+body {
+  width: 100%;
+  height: 100%;
+  font-family: "Roboto", sans-serif;
+  color: #000;
+}
+
+button {
+	cursor: pointer
+}
+`;
+
+export const Wrapper = styled.div`
+  width: 100%;
+  min-height: 100%;
+  overflow: hidden;
+  background-color: #FFF;;
+`
