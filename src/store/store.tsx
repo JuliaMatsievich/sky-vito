76e665c359a0f5478d6dda7dbf0096fc25d2fde2
JuliaMatsievich <juliamatsievich@mail.ru>
2023-12-14 15:26:00 +0500

    add redux toolkit

---
+++ src/store/store.tsx
@@ -0,0 +1,15 @@
+import { configureStore } from '@reduxjs/toolkit'
+// import  trackReducer  from './trackSlice'
+// import { trackApi } from '../services/trackApi'
+// import tokenReducer from './tokenSlice'
+import { userReducer } from './userSlice'
+
+
+export const store = configureStore({
+  reducer: {
+     user: userReducer,
+   //  [trackApi.reducerPath]: trackApi.reducer
+  },
+//   middleware: getDefaultMiddleware =>
+//   getDefaultMiddleware().concat(trackApi.middleware)
+})
---
+++ src/store/userSlice.tsx
@@ -0,0 +1,43 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+// function getUserFromLocalStorage() {
+// 	try {
+// 	  return JSON.parse(localStorage.getItem('user'));
+// 	} catch (error) {
+// 	  console.error(error);
+// 	  return null;
+// 	}
+//  }
+
+//  const initialState =  {
+// 	id: 0,
+// 	name: '',
+// 	first_name:'',
+// 	last_name: '',
+// 	email:''
+// }
+
+export const userSlice = createSlice({
+	name: 'user',
+	initialState: {
+		isUser: false
+	},
+	reducers: {
+		setUser(state, action) {
+			// const payload = action.payload ?? initialState;
+	
+			// state.id = payload.id;
+			// state.email = payload.email;
+			// state.username = payload.username;
+			// state.first_name = payload.first_name;
+			// state.last_name = payload.last_name;
+	
+			// localStorage.setItem('user', JSON.stringify(state));
+			state.isUser = action.payload
+		 },
+	}
+})
+
+export const { setUser } = userSlice.actions;
+export const userReducer = userSlice.reducer;
+
