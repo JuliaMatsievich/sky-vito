29957cd0d0af875bd5c7c31ff7061200ab1fffce
JuliaMatsievich <juliamatsievich@mail.ru>
2023-12-14 14:53:31 +0500

    init authpage

--- src/routes.tsx
+++ src/routes.tsx
@@ -5,18 +5,13 @@ import { ProfilePage } from './pages/profilePage';
 import { AdvertPage } from './pages/advertPage';
 import { ModalReviews } from './components/modal/modalReviews/modalReviews';
 import { AdvertSettings } from './components/modal/advertSettings/advertSettings';
+import { AuthPage } from './pages/authPage';
 
 export const AppRoutes = () => {
   return (
     <Routes>
-      {/* <Route
-			path="/login"
-			element={<AuthPage isLoginMode={true}></AuthPage>}
-		 ></Route>
-		 <Route
-			path="/register"
-			element={<AuthPage isLoginMode={false}></AuthPage>}
-		 ></Route> */}
+      <Route path="/login" element={<AuthPage />}></Route>
+      <Route path="/register" element={<AuthPage />}></Route>
       {/* <Route element={<ProtectedRoute />}> */}
       <Route path="/modalReviews" element={<ModalReviews />} />
       <Route path="/advertSettings" element={<AdvertSettings />} />
